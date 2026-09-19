ebdd8e546ed91c3bb599066199221a9bdb17ab2f
nandinimahajan2408-del <nandinimahajan2408@gmail.com>
2026-09-19 10:55:51 +0530

    Time: 27 ms (43.10%) | Memory: 139.1 MB (73.62%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-       vector<int>ans;
-       deque<int>dq;//stores index
+      vector<int>ans;
 
-       for(int i=0;i<nums.size();i++){
-        //check if it is in range or not else popfront
+      deque<int>dq;//storesidx
+      for(int i=0;i<nums.size();i++){
         if(!dq.empty()&&dq.front()<=i-k){
             dq.pop_front();
         }
-        while(!dq.empty()&&nums[dq.back()]<nums[i]){
+        while(!dq.empty()&&nums[dq.back()]<=nums[i]){
             dq.pop_back();
         }
         dq.push_back(i);
-
         if(i>=k-1) ans.push_back(nums[dq.front()]);
-       }
-       return ans;
+      }
+      return ans;
     }
 };
